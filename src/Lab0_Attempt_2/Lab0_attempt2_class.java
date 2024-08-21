465103d1b1b1e4fa52afe1fa2d41d0d05fa0a20e
Jacob <Jacob@10.97.233.81>
2024-08-21 15:27:37 -0400

    First commit!

--- src/Lab0_Attempt_2/Lab0_attempt2_class.java
+++ src/Lab0_Attempt_2/Lab0_attempt2_class.java
@@ -1,5 +1,12 @@
 package Lab0_Attempt_2;
 
-public class Lab0_attempt2_class {
+public class Lab0_attempt2_class 
+{
+
+		public static void main(String[] args) 
+		{
+			System.out.println("Hello world!");
+			System.out.println("Real G's move in silence like lasagna");
+		}
 
 }
