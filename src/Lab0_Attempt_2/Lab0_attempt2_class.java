46df9c1168a527a1307af857761c587d2e7e7752
Jacob <Jacob@10.97.234.135>
2024-08-30 19:35:23 -0400

    GetMax development for probability calculations

--- src/Lab0_Attempt_2/Lab0_attempt2_class.java
+++ src/Lab0_Attempt_2/Lab0_attempt2_class.java
@@ -5,6 +5,42 @@ import java.util.*;
 public class Lab0_attempt2_class 
 {
 
+		public static long getMax(double probability)
+		{
+			if (probability >= 0.001)
+			{
+				if (probability >= 0.01)
+				{
+					if (probability >= 0.1)
+					{
+						return 10;
+					}
+					else
+					{
+					return 100;
+					}
+				}
+				else
+				{
+					return 1000;
+				}
+			}
+			else
+			{
+				String stringProb = probability + "";
+				String mult = stringProb.substring(stringProb.indexOf("E") + 2, stringProb.length());
+				int multInt = Integer.parseInt(mult);
+					
+				long max = 1;
+				for (int count = 0; count < multInt; count++)
+				{
+					max *= 10;
+				}
+				return max;
+			}
+		}
+		
+		
 		public static String generateTriplet(Dictionary nucDict)
 		{
 			Random random = new Random();
@@ -52,6 +88,12 @@ public class Lab0_attempt2_class
 		}
 		
 		
+		public static Dictionary alterProbability(int aFreq, int tFreq, int cFreq, int gFreq, Dictionary originalDict)
+		{
+			getMax(aFreq);
+			
+		}
+		
 		
 		public static void main(String[] args) 
 		{
@@ -76,8 +118,25 @@ public class Lab0_attempt2_class
 			Dictionary baseNumbers = new Hashtable();
 			baseNumbers = countTriplets(baseString);
 			countBases(trackTriplet, baseNumbers);
+			
+			double cool = 0.0007;
+			
+			long reallyCool = getMax(cool);
+			
+			System.out.println(reallyCool);
+			System.out.println(getMax(0.001));
+			System.out.println(getMax(0.01));
+			System.out.println(getMax(0.1));
+
+			System.out.println(getMax(0.000000000000000001));
+
+
+			
 		}
 
-		
+/* QUESTIONS
+ * 	
+ * Assuming each base has an equal chance to appear, we would expect the AAA sequence to appear (also considering only one ORF) with a ratio of 1/81 
+ */
 
 }
