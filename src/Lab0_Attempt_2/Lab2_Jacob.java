ddfb2e84e50d94599a3809dadc2542c31a39c69e
Jacob <Jacob@DeepThought.attlocal.net>
2024-09-11 19:15:58 -0400

    Added time customization

--- src/Lab0_Attempt_2/Lab2_Jacob.java
+++ src/Lab0_Attempt_2/Lab2_Jacob.java
@@ -21,33 +21,45 @@ public class Lab2_Jacob
 		{ "A","R", "N", "D", "C", "Q", "E", 
 		"G",  "H", "I", "L", "K", "M", "F", 
 		"P", "S", "T", "W", "Y", "V" };
+		
+	public static Scanner userInput = new Scanner(System.in);
+
+	public static Random random = new Random();
 	
-	public static int TIME = 30;
-
+	
+	// Method for running the whole game
 	public static void beginGame()
-	{
-		System.out.println("LET THE GAMES BEGIN! You have " + TIME + " seconds pitiful human!\n");
-
-		Random random = new Random();
-	    Scanner aminoEntry = new Scanner(System.in);
-	    
-	    boolean contGame = true;
+	{		
+		boolean contGame = true;
 	    int score = 0;
-	    
+		
+	    // Get the amount of time for the game to run
+		System.out.println("Welcome to my command line window pathetic human. How many seconds would you like for your trial to last?");
+		int time = userInput.nextInt();
 		long start = System.currentTimeMillis();
-		long end = start + TIME * 1000;
+		long end = start + time * 1000;
+		userInput.nextLine(); // for making the scanner go to the next line so that it does not get input into userEntry below
+
+		
+		// Game start
+		System.out.println("\nLET THE GAMES BEGIN! You have " + time + " seconds insolet cur!\n");
 		while (System.currentTimeMillis() < end && contGame == true) 
 		{
+			
+			// Generate random amino acid
 			int amino = random.nextInt(FULL_NAMES.length - 1);
 			
+			
+			// User answer
 			System.out.println("FOOL! give me the amino acid abbreviation for " + FULL_NAMES[amino].toUpperCase() + "!");
-		    String userEntry = (aminoEntry.nextLine()).toUpperCase();
-		    		    
+		    String userEntry = (userInput.nextLine()).toUpperCase();
+		    
+		    
+		    // Answer check
 		    if (SHORT_NAMES[amino].equals(userEntry.toUpperCase()))
 		    {
 		    	score += 1;
-		    	System.out.println("\nGah! That's right! Don't get too comfortable! You have only scored " + score + " points in " + (TIME - (end - System.currentTimeMillis())/1000) + " seconds.");
-		    	
+		    	System.out.println("\nGah! That's right! Don't get too comfortable! You have only scored " + score + " points in " + (time - (end - System.currentTimeMillis())/1000) + " seconds.");
 		    }
 		    else
 		    {
@@ -56,8 +68,12 @@ public class Lab2_Jacob
 		    }
 		 
 		}
-    	System.out.println("Your PUNY human brain was able to achieve a score of " + score + " points!");
+    	
+		// Final score
+		System.out.println("Your PUNY human brain was able to achieve a score of " + score + " points!");
 	}
+	
+	
 	
 	public static void main(String[] args)
 	{
