6a28dd36bded918174077c37ea6bc64854f531d0
Jacob <Jacob@Gir.attlocal.net>
2024-09-29 17:45:10 -0400

    Wrote MORE OF THE LAB

--- src/Lab0_Attempt_2/Lab4/FastaSequence.java
+++ src/Lab0_Attempt_2/Lab4/FastaSequence.java
@@ -1,5 +1,6 @@
 package Lab0_Attempt_2.Lab4;
 
+import java.io.*;
 import java.util.*;
 
 public class FastaSequence
@@ -16,25 +17,70 @@ public class FastaSequence
 	
 	public String getHeader()
 	{
-		return this.header;
+		return header.substring(1);
 	}
 	
 	public String getSequence()
 	{
-		return this.sequence;
+		return sequence;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return (header + "\n" + sequence );
 	}
 
 	public float getGCRatio()
 	{
 		int numGC = 0;
-		for (int i = 0; i < this.sequence.length(); i++)
+		for (int i = 0; i < sequence.length(); i++)
 		{
-			if (this.sequence.charAt(i) == 'C' || this.sequence.charAt(i) == 'G')
+			if (sequence.charAt(i) == 'C' || sequence.charAt(i) == 'G')
 				{
 					numGC += 1;
 				}
-			return ((float)numGC)/sequence.length();
 		}
+		return ((float)numGC)/(sequence.length());
 	}
+	
+	public static List<FastaSequence> readFastaFile(String filepath) throws Exception
+	{
+		BufferedReader fileContent = new BufferedReader(new FileReader(new File(filepath)));
+		List<FastaSequence> FSList = new ArrayList<FastaSequence>();
+		StringBuffer tempHeader = new StringBuffer();
+		StringBuffer tempSeq = new StringBuffer();
+		
+		String nextLine = fileContent.readLine();
+		tempHeader.append(nextLine);
+		
+        for (nextLine = fileContent.readLine(); nextLine != null; nextLine = fileContent.readLine())
+        {
+
+        	if (nextLine.isEmpty())
+        	{
+        		;
+        	}
+        	else if (nextLine.charAt(0) == '>' && nextLine.isEmpty() != true)
+        	{
+        		FSList.add(new FastaSequence(tempHeader.toString(), tempSeq.toString()));
+        		tempHeader.setLength(0);
+        		tempSeq.setLength(0);
+        		tempHeader.append(nextLine);
+        	}
+        	
+        	else
+        	{
+        		tempSeq.append(nextLine);
+        	}
+        }
+        
+        fileContent.close();
+        
+        FSList.add(new FastaSequence(tempHeader.toString(), tempSeq.toString())); 
+        return FSList;
+        
+	}
+	
 }
 
--- src/Lab0_Attempt_2/Lab4/Lab4_Main.java
+++ src/Lab0_Attempt_2/Lab4/Lab4_Main.java
@@ -1,6 +1,7 @@
 package Lab0_Attempt_2.Lab4;
 
 import java.io.*;
+import java.util.*;
 
 /*
 public class FastaSequence
@@ -15,19 +16,55 @@ public class FastaSequence
 
 public class Lab4_Main 
 {
+	
+	public static void writeTableSummary(List<FastaSequence> list, File outputFile) throws Exception
+	{
+		StringBuffer seq = new StringBuffer();
+		
+		StringBuffer fastaRow = new StringBuffer("sequenceID\tnumA\tnumC\tnumG\tnumT\tsequence\n");
+		
+		
+		BufferedWriter writeOutput = new BufferedWriter(new FileWriter(outputFile));
+		
+		writeOutput.write(fastaRow.toString());
+		
+		
+		for (FastaSequence entry : list)
+		{
+			writeOutput.write(entry.getHeader());
+			seq.append(entry.getSequence());
+			for (int i = 0; i < seq.length(); i++)
+			{
+				; // Write something to count each base in the sequence (.charAt())
+			}
+		}
+		writeOutput.close();
+		
+		outputFile.createNewFile();
+
+	}
+	
+	
 	public static void main(String[] args) throws Exception
 	{
 		System.out.println("Hello world!");
         
-        BufferedReader fileContent = new BufferedReader(new FileReader(new File("C:\\Users\\Jacob\\Desktop\\Wormhole\\Yohe_Lab\\NonBatMasterFasta\\Canis_lupus.newname.tblastn.fa")));
-        int numLines = 0;
-        
-        for (String nextLine = fileContent.readLine(); nextLine != null; nextLine = fileContent.readLine())
-        {
-        	numLines += 1;
-        	System.out.println(nextLine + "\t line number " + numLines);
-        }        
-	
-        FastaSequence seq = new FastaSequence("WOW", "agct");
+        //BufferedReader fileContent = new BufferedReader(new FileReader(new File("C:\\Users\\Jacob\\Desktop\\Wormhole\\Yohe_Lab\\NonBatMasterFasta\\Canis_lupus.newname.tblastn.fa")));
+
+		List<FastaSequence> mainFSList = FastaSequence.readFastaFile("G:\\Other computers\\DeepThought\\Wormhole\\Yohe_Lab\\NonBatMasterFasta\\Canis_lupus.newname.tblastn.fa");
+		
+		for (FastaSequence x : mainFSList)
+		{
+			System.out.println("\n" + x);
+		}
+		
+		
+		File outputFile = new File("C:\\Users\\Jacob\\Desktop\\outputFastaTable.txt");
+		
+		FastaSequence seq = new FastaSequence("WOW", "agGCct");
+		
+		writeTableSummary(mainFSList, outputFile);
+		
+		System.out.println(seq.getGCRatio());
 	}
 }
