c8db859346096917289fe4b7cd6d1f51e5f8aac5
Jacob <Jacob@Gir.attlocal.net>
2024-09-30 18:29:37 -0400

    Lab 4 done I think!

--- src/Lab0_Attempt_2/Lab4/FastaSequence.java
+++ src/Lab0_Attempt_2/Lab4/FastaSequence.java
@@ -7,17 +7,16 @@ public class FastaSequence
 {
 	private final String header;
 	private final String sequence;
-
 	
 	public FastaSequence(String header, String sequence)
 	{
 		this.header = header;
-		this.sequence = sequence.toUpperCase();
+		this.sequence = sequence.toUpperCase(); // Just to keep case consistent
 	}
 	
 	public String getHeader()
 	{
-		return header.substring(1);
+		return header;
 	}
 	
 	public String getSequence()
@@ -31,6 +30,7 @@ public class FastaSequence
 		return (header + "\n" + sequence );
 	}
 
+	// Gets the ratio of G and C nucleotides relative to the length of a sequence
 	public float getGCRatio()
 	{
 		int numGC = 0;
@@ -44,43 +44,45 @@ public class FastaSequence
 		return ((float)numGC)/(sequence.length());
 	}
 	
+	// Static factory method using FastaSequences from a file
 	public static List<FastaSequence> readFastaFile(String filepath) throws Exception
 	{
 		BufferedReader fileContent = new BufferedReader(new FileReader(new File(filepath)));
 		List<FastaSequence> FSList = new ArrayList<FastaSequence>();
+		
+		// StringBuffers used to avoid creating a bunch of string objects
 		StringBuffer tempHeader = new StringBuffer();
 		StringBuffer tempSeq = new StringBuffer();
 		
-		String nextLine = fileContent.readLine();
+		String nextLine = fileContent.readLine(); 	// The logic of the loop will miss the first sequence if the first line is not read in before the loop
 		tempHeader.append(nextLine);
 		
+		// Reads through whole file
         for (nextLine = fileContent.readLine(); nextLine != null; nextLine = fileContent.readLine())
         {
-
+        	// Does nothing if a line is empty or just contains "\n"
         	if (nextLine.isEmpty())
         	{
         		;
         	}
-        	else if (nextLine.charAt(0) == '>' && nextLine.isEmpty() != true)
+        	
+        	// If line is a header
+        	else if (nextLine.charAt(0) == '>')
         	{
-        		FSList.add(new FastaSequence(tempHeader.toString(), tempSeq.toString()));
+        		FSList.add(new FastaSequence(tempHeader.toString().substring(1), tempSeq.toString()));
         		tempHeader.setLength(0);
         		tempSeq.setLength(0);
         		tempHeader.append(nextLine);
         	}
         	
+        	// If line is a part of the coding sequence
         	else
         	{
-        		tempSeq.append(nextLine);
+        		tempSeq.append(nextLine.replace("\n", ""));
         	}
         }
-        
+        FSList.add(new FastaSequence(tempHeader.toString(), tempSeq.toString()));  // Adds the last entry since it's missed by the loop
         fileContent.close();
-        
-        FSList.add(new FastaSequence(tempHeader.toString(), tempSeq.toString())); 
         return FSList;
-        
 	}
-	
 }
-
--- src/Lab0_Attempt_2/Lab4/Lab4_Main.java
+++ src/Lab0_Attempt_2/Lab4/Lab4_Main.java
@@ -3,68 +3,58 @@ package Lab0_Attempt_2.Lab4;
 import java.io.*;
 import java.util.*;
 
-/*
-public class FastaSequence
-{
-	public FastaSequence(header, sequence)
-	{
-		this.header = header;
-		this.sequence = sequence;
-	}
-}
-*/
-
 public class Lab4_Main 
 {
 	
+	// Method for taking a list  of FastaSequence objects and converting them into a spreadsheet, counting each nucleotide
 	public static void writeTableSummary(List<FastaSequence> list, File outputFile) throws Exception
-	{
-		StringBuffer seq = new StringBuffer();
-		
-		StringBuffer fastaRow = new StringBuffer("sequenceID\tnumA\tnumC\tnumG\tnumT\tsequence\n");
-		
-		
+	{		
 		BufferedWriter writeOutput = new BufferedWriter(new FileWriter(outputFile));
+		HashMap<Character, Integer> nucCounts = new HashMap<>(5); // Using 5 buckets ensures each nucleotide gets it's own bucket (0, 2, 1, 4)
+		nucCounts.put('A', 0);
+		nucCounts.put('C', 0);
+		nucCounts.put('G', 0);
+		nucCounts.put('T', 0);
 		
-		writeOutput.write(fastaRow.toString());
+		// Header
+		writeOutput.write("sequenceID\tnumA\tnumC\tnumG\tnumT\tsequence\n");
 		
-		
+		// Loop through each FastaSequence Object
 		for (FastaSequence entry : list)
 		{
-			writeOutput.write(entry.getHeader());
-			seq.append(entry.getSequence());
-			for (int i = 0; i < seq.length(); i++)
+			
+			// Loop for going through each nucleotide sequence and counting the bases, updates hashmap based on nucleotide
+			for (int i = 0; i < entry.getSequence().length(); i++)
 			{
-				; // Write something to count each base in the sequence (.charAt())
+				nucCounts.replace(entry.getSequence().charAt(i), nucCounts.get(entry.getSequence().charAt(i)) + 1);
 			}
+			// Write output
+			writeOutput.write(entry.getHeader() + "\t" + nucCounts.get('A') + "\t" + nucCounts.get('C') + "\t" + nucCounts.get('G') + "\t" + nucCounts.get('G') + "\t" + entry.getSequence() + "\n");
+			
+			// Reset the hash map for the next sequence
+			nucCounts.replace('A', 0);
+			nucCounts.replace('C', 0);
+			nucCounts.replace('G', 0);
+			nucCounts.replace('T', 0);
 		}
-		writeOutput.close();
-		
 		outputFile.createNewFile();
-
+		writeOutput.close();
 	}
 	
 	
+	// Main method
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Hello world!");
-        
-        //BufferedReader fileContent = new BufferedReader(new FileReader(new File("C:\\Users\\Jacob\\Desktop\\Wormhole\\Yohe_Lab\\NonBatMasterFasta\\Canis_lupus.newname.tblastn.fa")));
-
 		List<FastaSequence> mainFSList = FastaSequence.readFastaFile("G:\\Other computers\\DeepThought\\Wormhole\\Yohe_Lab\\NonBatMasterFasta\\Canis_lupus.newname.tblastn.fa");
 		
-		for (FastaSequence x : mainFSList)
+		for (FastaSequence fs : mainFSList)
 		{
-			System.out.println("\n" + x);
+			System.out.println(fs.getHeader());
+			System.out.println(fs.getSequence());
+			System.out.println(fs.getGCRatio());
 		}
 		
-		
 		File outputFile = new File("C:\\Users\\Jacob\\Desktop\\outputFastaTable.txt");
-		
-		FastaSequence seq = new FastaSequence("WOW", "agGCct");
-		
 		writeTableSummary(mainFSList, outputFile);
-		
-		System.out.println(seq.getGCRatio());
 	}
 }
